0d8533c13bc0bba9e2470f326b00f6aaeebc718b
lkimasu <lwlstjdl@naver.com>
2024-02-23 09:33:00 +0900

    add event

--- src/ApartmentManagementProgram.java
+++ src/ApartmentManagementProgram.java
@@ -1,12 +1,11 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
 
 public class ApartmentManagementProgram {
     public static void createAndShowMainGUI() {
         JFrame frame = new JFrame("주민 관리 프로그램");
-        frame.setSize(1500, 500);
+        frame.setSize(800, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
@@ -19,57 +18,44 @@ public class ApartmentManagementProgram {
     private static void placeComponents(JPanel panel) {
         panel.setLayout(null);
 
-        JButton signUpButton = new JButton("회원가입");
+        // 주민등록 버튼
+        JButton signUpButton = new JButton("주민등록");
         signUpButton.setBounds(10, 20, 120, 25);
         panel.add(signUpButton);
 
+        // 주민등록 버튼에 대한 액션 리스너 추가
         signUpButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // 회원가입 창 열기
-                join.createAndShowMainGUI();
+                // 주민등록 이벤트
             }
         });
-        
 
+        // 주민정보 확인 버튼
         JButton residentInfoButton = new JButton("주민정보 확인");
         residentInfoButton.setBounds(140, 20, 120, 25);
         panel.add(residentInfoButton);
 
+        // 관리비 입력 버튼
         JButton feeInputButton = new JButton("관리비 입력");
         feeInputButton.setBounds(270, 20, 120, 25);
         panel.add(feeInputButton);
 
+        // 관리비 정보 버튼
         JButton feeInfoButton = new JButton("관리비 정보");
         feeInfoButton.setBounds(400, 20, 120, 25);
         panel.add(feeInfoButton);
 
+        // 알림 보내기 버튼
         JButton sendNotificationButton = new JButton("알림 보내기");
         sendNotificationButton.setBounds(530, 20, 120, 25);
         panel.add(sendNotificationButton);
 
-        // 주민 커뮤니티 포럼 버튼
+        // 커뮤니티 포럼 버튼
         JButton communityForumButton = new JButton("커뮤니티 포럼");
         communityForumButton.setBounds(660, 20, 120, 25);
         panel.add(communityForumButton);
-
-        // 시설 예약 버튼
-        JButton facilityReservationButton = new JButton("시설 예약");
-        facilityReservationButton.setBounds(790, 20, 120, 25);
-        panel.add(facilityReservationButton);
-
-        // 리마인더 버튼
-        JButton reminderButton = new JButton("리마인더");
-        reminderButton.setBounds(920, 20, 120, 25);
-        panel.add(reminderButton);
-
-        // 이벤트 캘린더 버튼
-        JButton eventCalendarButton = new JButton("이벤트 캘린더");
-        eventCalendarButton.setBounds(1050, 20, 120, 25);
-        panel.add(eventCalendarButton);
     }
-
-    
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(ApartmentManagementProgram::createAndShowMainGUI);
