eccb1da215a1f352798cef699623bd3f392c46b3
lkimasu <lwlstjdl@naver.com>
2024-02-23 16:12:57 +0900

    update ui

--- src/ApartmentManagementProgram.java
+++ src/ApartmentManagementProgram.java
@@ -5,25 +5,27 @@ import javax.swing.*;
 public class ApartmentManagementProgram {
     public static void createAndShowMainGUI() {
         JFrame frame = new JFrame("주민 관리 프로그램");
-        frame.setSize(800, 500);
+        frame.setSize(550, 250);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
         frame.add(panel);
         placeComponents(panel);
 
+        // 프레임을 화면 중앙에 배치
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
     private static void placeComponents(JPanel panel) {
         panel.setLayout(null);
 
-        // 주민등록 버튼
-        JButton signUpButton = new JButton("주민등록");
+        // 회원가입 버튼
+        JButton signUpButton = new JButton("회원가입");
         signUpButton.setBounds(10, 20, 120, 25);
         panel.add(signUpButton);
 
-        // 주민등록 버튼에 대한 액션 리스너 추가
+        // 회원가입 버튼에 대한 액션 리스너 추가
         signUpButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -31,8 +33,8 @@ public class ApartmentManagementProgram {
             }
         });
 
-        // 주민정보 확인 버튼
-        JButton residentInfoButton = new JButton("주민정보 확인");
+        // 주민정보 버튼
+        JButton residentInfoButton = new JButton("주민정보");
         residentInfoButton.setBounds(140, 20, 120, 25);
         panel.add(residentInfoButton);
 
@@ -48,12 +50,12 @@ public class ApartmentManagementProgram {
 
         // 알림 보내기 버튼
         JButton sendNotificationButton = new JButton("알림 보내기");
-        sendNotificationButton.setBounds(530, 20, 120, 25);
+        sendNotificationButton.setBounds(10, 60, 120, 25);
         panel.add(sendNotificationButton);
 
         // 커뮤니티 포럼 버튼
-        JButton communityForumButton = new JButton("커뮤니티 포럼");
-        communityForumButton.setBounds(660, 20, 120, 25);
+        JButton communityForumButton = new JButton("커뮤니티");
+        communityForumButton.setBounds(140, 60, 120, 25);
         panel.add(communityForumButton);
     }
 
